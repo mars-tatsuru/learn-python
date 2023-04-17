312052d399e08a186d7e13f0a3966df953298d07
小池　建 <t-koike@marsflag.com>
2023-04-17 12:51:15 +0900

    feature: learn class, super and relative import

--- sample.py
+++ sample.py
@@ -68,23 +68,123 @@ friends_age = [
 
 
 
-class Store:
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
-        self.items = []
+# class Store:
+#     def __init__(self, name, price):
+#         self.name = name
+#         self.price = price
+#         self.items = []
 
-    def add_item(self, name, price):
-        item = {"name": name, "price": price}
-        self.items.append(item)
+#     def add_item(self, name, price):
+#         item = {"name": name, "price": price}
+#         self.items.append(item)
 
-    def stock_price(self):
-        return sum([item["price"] for item in self.items])
+#     def stock_price(self):
+#         return sum([item["price"] for item in self.items])
 
-    def __str__(self):
-        return f"{self.name} has {len(self.items)} items."
+#     def __str__(self):
+#         return f"{self.name} has {len(self.items)} items."
 
-store = Store("tatsuru",100)
-print(store.add_item("tatsuru",100))
-print(store.stock_price())
-print(store.__str__())
+# store = Store("tatsuru",100)
+# print(store.add_item("tatsuru",100))
+# print(store.stock_price())
+# print(store.__str__())
+
+
+
+
+# class ClassTest:
+#     def instance_method(self):
+#         print(f"Called instance_method of {self}")
+
+#     @classmethod
+#     def class_method(cls):
+#         print(f"Called class_method of {cls}")
+
+#     @staticmethod
+#     def static_method():
+#         print("Called static_method")
+
+# # test = ClassTest()
+# # test.instance_method()
+# ClassTest.static_method()
+
+
+# class Book:
+#     TYPES = ("hardcover", "paperback")
+
+#     def __init__(self, name, book_type, weight):
+#         self.name = name
+#         self.book_type = book_type
+#         self.weight = weight
+
+#     def __repr__(self):
+#         return f"<Book {self.name}, {self.book_type}, weighing {self.weight}g>"
+
+#     @classmethod
+#     def hardcover(cls, name, page_weight):
+#         return cls(name, cls.TYPES[0], page_weight + 100)
+
+#     @classmethod
+#     def paperback(cls, name, page_weight):
+#         return cls(name, cls.TYPES[1], page_weight)
+
+
+# book = Book.hardcover("Harry Potter", 1500)
+# light = Book.paperback("Harry Potter", 1500)
+
+# print(book)
+# print(light)
+
+
+# class Device:
+#     def __init__(self, name, connected_by):
+#         self.name = name
+#         self.connected_by = connected_by
+#         self.connected = True
+
+#     def __str__(self):
+#         return f"Device {self.name!r} ({self.connected_by})"
+
+#     def disconnect(self):
+#         self.connected = False
+#         print("Disconnected.")
+
+# class Printer(Device):
+#     def __init__(self, name, connected_by, capacity):
+#         super().__init__(name, connected_by)
+#         self.capacity = capacity
+#         self.remaining_pages = capacity
+
+#     def __str__(self):
+#         return f"{super().__str__()} ({self.remaining_pages} pages remaining)"
+
+#     def print(self, pages):
+#         if not self.connected:
+#             print("Your printer is not connected!")
+#             return
+#         print(f"Printing {pages} pages.")
+#         self.remaining_pages -= pages
+
+
+# class BookShelf:
+#     def __init__(self, *books):
+#         self.books = books
+
+#     def __str__(self):
+#         return f"BookShelf with {len(self.books)} books."
+
+# class Book:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def __str__(self):
+#         return f"Book {self.name}"
+
+# book = Book("Harry Potter")
+# # book2 = Book("Python 101")
+# shelf = BookShelf(book)
+
+# print(shelf)
+
+
+
